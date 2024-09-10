1e38627520f260c2e6fdc9fccf902fd4d4fe0d20
Yandri Sanchez <ysanchez3d@gmail.com>
2024-09-10 10:53:40 -0700

    added messages conficuration

--- calculator.py
+++ calculator.py
@@ -1,21 +1,58 @@
-print('Welcome to the Calculator!')
+import json
 
-print("What's the first number?")
-number1 = input()
+with open('calculator_messages.json', 'r') as file:
+    data = json.load(file)
 
-print("What's the second number?")
-number2 = input()
+def prompt(message):
+    print(f"==> {message}")
 
-print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
-operation = input()
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
 
-if operation == '1':   # '1' represents addition
-    output = int(number1) + int(number2)
-elif operation == '2': # '2' represents subtraction
-    output = int(number1) - int(number2)
-elif operation == '3': # '3' represents multiplication
-    output = int(number1) * int(number2)
-elif operation == '4': # '4' represents division
-    output = int(number1) / int(number2)
+    return False
 
-print(f"The result is: {output}")
+def perform_calculation():
+    prompt("What's the first number?")
+    number1 = input()
+
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
+
+    prompt("What's the second number?")
+    number2 = input()
+
+    while invalid_number(number2):
+        prompt(data["invalid"])
+        number2 = input()
+
+    prompt(data["operation"])
+    operation = input()
+
+    while operation not in ["1", "2", "3", "4"]:
+        prompt(data["pick_operation"])
+        operation = input()
+
+    match operation:
+        case "1":
+            output = int(number1) + int(number2)
+        case "2":
+            output = int(number1) - int(number2)
+        case "3":
+            output = int(number1) * int(number2)
+        case "4":
+            output = int(number1) / int(number2)
+
+    prompt(f"The result is {output}")
+
+    prompt(data["new_calculation"])
+    answer = input()
+
+    if answer == 'yes':
+        perform_calculation()
+
+prompt(data["welcome"])
+perform_calculation()
